79be4aebd0f7a260b8cd595deb33db2bedb63e82
devoteddddddd <99391825+devoteddddddd@users.noreply.github.com>
2023-06-03 11:09:14 +0800

    .

--- Home_page.py
+++ Home_page.py
@@ -1,9 +1,34 @@
 
 import streamlit as st
-import matplotlib.pyplot as plt
-import numpy as np
-arr = np.random.normal(1, 1, size=100)
-plt.hist(arr, bins=20)
+import joblib
+import pandas as pd
 
-st.pyplot()
+
+df = pd.read_csv('make_up.csv')
+l = list(d['text2'])
+
+idx_to_str = {0:'Negative', 1:'Positive'}
+
+st.set_page_config(page_title="Emotion prediction")
+st.title('Emotion prediction')
+content = st.text_area('Please enter an English comment (only one comment can be predicted at a time)', key = 0)
+
+
+
+if st.button('Run', key=1):
+    with st.spinner('The system is loading and inferring model, please wait...'):
+        @st.cache(hash_funcs={"MyUnhashableClass": lambda _: None}, allow_output_mutation=True)
+        def load_model():
+            return joblib.load('./RFModel.pkl'), joblib.load('./Vectorizer.pkl')
+
+        model, vectorizer = load_model()
+
+    l.append(content)
+    embed = vectorizer.fit_transform(l)
+    e = list(embed.toarray())
+    p = model.predict(e)
+    result = p[-1]
+    output = idx_to_str[result]
+    st.write('The predicted emotion is', output)
+    st.success('Model loading and inference successful!')
 
